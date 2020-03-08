eaea22b2ee8027da3d913e8752a69712ec7ea41c
risingbirdsong <heregoingthere@outlook.com>
2020-03-08 11:05:36 -0700

    want to be able to switch on names of options and only display one at a time, not quit there but this is first attempt

--- reactPlay/canvas-play-ts/src/App.tsx
+++ reactPlay/canvas-play-ts/src/App.tsx
@@ -12,26 +12,41 @@ function App() {
   const [fishHookToggle, setFishHook] = useState(false);
   const [hexaToggle, setHexa] = useState(false);
   const [animationToggle, setAnimation] = useState(false);
+
+  const [currentDisplay, setDisplay] = useState("");
+
   return (
     <div>
       <h1>welcome</h1>
-      <button onClick={() => setFishHook(!fishHookToggle)}>fish hooh</button>
-      <button onClick={() => setHexa(!hexaToggle)}> hexa </button>
-      <button onClick={() => setAnimation(!animationToggle)}> animation </button>
+      <button onClick={() => { setFishHook(!fishHookToggle); setDisplay("fishHook") }}>fish hooh</button>
+      <button onClick={() => { setHexa(!hexaToggle); setDisplay("hexa") }}> hexa </button>
+      <button onClick={() => { setAnimation(!animationToggle); setDisplay("animation") }}> animation </button>
+
+      <div>
+        <h2>set display test -> {currentDisplay}</h2>
+      </div>
+
 
       {fishHookToggle ? <FishHook /> : null}
       {hexaToggle ? <Hexa /> : null}
-      <Animation rateOfRotate={1} />
-      <Animation rateOfRotate={2} />
-      <Animation rateOfRotate={3} />
-      <Animation rateOfRotate={4} />
-      <Animation rateOfRotate={5} />
-      <Animation rateOfRotate={6} />
-      <Animation rateOfRotate={7} />
-      <Animation rateOfRotate={8} />
-      <Animation rateOfRotate={9} />
-      <Animation rateOfRotate={15} />
-      <Animation rateOfRotate={20} />
+      {animationToggle ? <div>
+        <h2>all these rotations</h2>
+        <Animation rateOfRotate={1} />
+        <Animation rateOfRotate={2} />
+        <Animation rateOfRotate={3} />
+        <Animation rateOfRotate={4} />
+        <Animation rateOfRotate={5} />
+        <Animation rateOfRotate={6} />
+        <Animation rateOfRotate={7} />
+        <Animation rateOfRotate={8} />
+        <Animation rateOfRotate={9} />
+        <Animation rateOfRotate={10} />
+        <Animation rateOfRotate={11} />
+        <Animation rateOfRotate={12} />
+        <Animation rateOfRotate={13} />
+        <Animation rateOfRotate={14} />
+        <Animation rateOfRotate={15} />
+      </div> : null}
     </div>
   );
 }
