0abd059178c1227bbf58d2ba0b2c7bd87d1aa4f2
risingbirdsong <heregoingthere@outlook.com>
2020-03-10 17:56:32 -0700

    used useEffects to change the color on the canvas with a useState

--- reactPlay/canvas-play-ts/src/components/testComponent.tsx
+++ reactPlay/canvas-play-ts/src/components/testComponent.tsx
@@ -1,25 +1,29 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 const TestComponent = () => {
+
+  const [curColor, setColor] = useState("blue");
+
   const canvasRef = useRef(null);
   useEffect(() => {
     const canvas = canvasRef.current as HTMLCanvasElement;
     const ctx = canvas.getContext('2d');
     //last commit change that fixed it and now loads on page load... 
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = curColor;
     ctx.fillRect(10, 10, 300, 300)
-  }, [])
+  })
   return (
     <canvas
       ref={canvasRef}
       width={window.innerWidth / 2}
       height={window.innerHeight / 2}
-    // onClick={() => {
-    //   const canvas = canvasRef.current as HTMLCanvasElement;
-    //   const ctx = canvas.getContext('2d');
-    //   ctx.fillStyle = "blue";
-    //   ctx.fillRect(10, 10, 300, 300)
-    // }}
+      onClick={() => {
+        const canvas = canvasRef.current as HTMLCanvasElement;
+        const ctx = canvas.getContext('2d');
+        setColor("red");
+        // ctx.fillStyle = curColor;
+        // ctx.fillRect(10, 10, 300, 300)
+      }}
     />
   )
 }
