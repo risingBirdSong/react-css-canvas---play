64cfb6f59b025978ab30b9e74a9c4d1b710f5b6a
risingbirdsong <heregoingthere@outlook.com>
2020-03-14 21:01:23 -0700

    cool, incorporating pi and colors into making circles

--- reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
+++ reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState, useRef } from "react";
+import CSS_COLOR_NAMES from "../colors";
 
 const Canvassing = () => {
   const myRef = useRef();
   let angle = 0;
+  let color = CSS_COLOR_NAMES[0];
+
   useEffect(() => {
     let canvas = myRef.current as HTMLCanvasElement;
     let ctx = canvas.getContext("2d");
@@ -10,25 +13,35 @@ const Canvassing = () => {
 
     let centerX = canvas.width / 2;
     let centerY = canvas.height / 2;
-
+    let angleTracker = 0;
+    let rotation360 = 0;
+    let piTracker = 0;
     const render = () => {
       // ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.beginPath();
+
+      if (piTracker >= Math.PI * 2) {
+        return cancelAnimationFrame(requestId);
+      }
       // ctx.moveTo(
       //   centerX + (Math.cos(angle) * 35 + 35),
       //   centerY + (Math.cos(angle) * 35 + 35)
       // );
-      // Math.cos(angle) * 50
+      console.log(piTracker, "piTracker");
       ctx.fillRect(
         centerX + Math.sin(angle) * 50,
         centerY + Math.cos(angle) * 50,
         4,
         4
       ); // fill in the pixel at (10,10)
-      ctx.fillStyle = "red";
+      ctx.fillStyle =
+        CSS_COLOR_NAMES[Math.floor(CSS_COLOR_NAMES.length * Math.random())];
       ctx.fill();
       ctx.closePath();
-      angle += 0.05;
+      let amount = 0.05;
+      angle += amount;
+      piTracker += amount;
+      angleTracker += angle;
       requestId = requestAnimationFrame(render);
     };
     render();
