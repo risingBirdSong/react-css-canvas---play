a7a91553e2071e3dcf8f49b678da39615a72e457
risingbirdsong <heregoingthere@outlook.com>
2020-03-15 12:42:40 -0700

    thankfully i was able to fix it, now it's nice and centered, the problem was that i was translating intside the animation... the fix is the translation outside the animation, it translates once, and that does it :)

--- reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
+++ reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
@@ -5,6 +5,7 @@ const Canvassing = () => {
   const myRef = useRef();
   let angle = 0;
   let color = purpleArr[0];
+  let debug = 10;
 
   useEffect(() => {
     let canvas = myRef.current as HTMLCanvasElement;
@@ -13,12 +14,13 @@ const Canvassing = () => {
 
     let centerX = canvas.width / 2;
     let centerY = canvas.height / 2;
+    ctx.beginPath();
+    ctx.translate(centerX, centerY);
+    // ctx.moveTo(centerX, centerY);
 
     const render = () => {
-      ctx.beginPath();
-      ctx.moveTo(centerX, centerY);
       ctx.lineWidth = 3;
-      ctx.lineTo(centerX + 10, centerY + 10);
+      ctx.lineTo(200, 200);
       ctx.rotate(Math.PI / 45);
       ctx.strokeStyle = purpleArr[Math.floor(purpleArr.length * Math.random())];
       ctx.stroke();
