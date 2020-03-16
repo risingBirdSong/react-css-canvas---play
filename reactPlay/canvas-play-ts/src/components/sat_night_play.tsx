6c28408671a4908aa0ed6f5cc0f52424ba86ff2a
risingbirdsong <heregoingthere@outlook.com>
2020-03-16 15:48:45 -0700

    so cool with a middle ring, by changing the tan multiplier equal to the cos multiplier

--- reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
+++ reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
@@ -38,7 +38,7 @@ const Canvassing = () => {
       let rotator = Math.PI / 45;
       // ctx.lineTo(rotatorTracker, rotatorTracker);
       console.log("crazy lotus fractal pattern");
-      ctx.lineTo(Math.cos(piRotator) * 300, Math.tanh(piRotator) * 50);
+      ctx.lineTo(Math.cos(piRotator) * 300, Math.tanh(piRotator) * 300);
 
       rotator = Number(rotator.toFixed(4));
       rotatorTracker += rotator;
