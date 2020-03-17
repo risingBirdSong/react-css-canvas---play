71a4945dbe500634fe9708638af096e9f1f53234
risingbirdsong <heregoingthere@outlook.com>
2020-03-17 09:31:30 -0700

    anvil maze

--- reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
+++ reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
@@ -39,7 +39,13 @@ const Canvassing = () => {
       let rotator = Math.PI / 10;
       // ctx.lineTo(rotatorTracker, rotatorTracker);
       console.log("crazy lotus fractal pattern");
-      ctx.arc(pieTracker, pieTracker / piRotator, piRotator, 0, Math.PI * 2);
+      ctx.arc(
+        pieTracker * piRotator,
+        pieTracker / piRotator,
+        piRotator,
+        0,
+        Math.PI * 2
+      );
 
       rotator = Number(rotator.toFixed(4));
       rotatorTracker += rotator;
