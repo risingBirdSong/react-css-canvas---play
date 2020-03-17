5d9ac50fe3ef0db4a0cd531cb56006f31a8b911d
risingbirdsong <heregoingthere@outlook.com>
2020-03-16 17:09:10 -0700

    cool effect when i divide by 90

--- reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
+++ reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
@@ -30,12 +30,12 @@ const Canvassing = () => {
         piRotator = 0;
       }
 
-      if (rotatorTracker > 3.141592 * 6) {
+      if (rotatorTracker > 3.141592 * 2) {
         console.log("pieTracker", pieTracker, "rotatorTracker", rotatorTracker);
         return cancelAnimationFrame(requestId);
       }
       ctx.lineWidth = 3;
-      let rotator = Math.PI / 43;
+      let rotator = Math.PI / 90;
       // ctx.lineTo(rotatorTracker, rotatorTracker);
       console.log("crazy lotus fractal pattern");
       ctx.lineTo(
