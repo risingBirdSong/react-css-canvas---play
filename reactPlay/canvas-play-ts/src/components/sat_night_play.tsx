9dfcc57e1eccbba3bf5c1f00f5a6aa4e2d6f8aa8
risingbirdsong <heregoingthere@outlook.com>
2020-03-16 17:55:38 -0700

    interesting 5 paths intersecting

--- reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
+++ reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
@@ -41,7 +41,7 @@ const Canvassing = () => {
       console.log("crazy lotus fractal pattern");
       ctx.lineTo(
         Math.cos(piRotator),
-        Math.sin(piRotator) ** Math.tanh(piRotator) * 200
+        (Math.tanh(piRotator) / Math.sin(piRotator)) * 50
       );
 
       rotator = Number(rotator.toFixed(4));
