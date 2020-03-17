b38f26f83c7bad1ff2f77e638fac386667ee646b
risingbirdsong <heregoingthere@outlook.com>
2020-03-16 23:28:14 -0700

    targeting system

--- reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
+++ reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
@@ -39,7 +39,7 @@ const Canvassing = () => {
       let rotator = Math.PI / 3;
       // ctx.lineTo(rotatorTracker, rotatorTracker);
       console.log("crazy lotus fractal pattern");
-      ctx.lineTo(pieTracker * 20 * Math.cos(piRotator), piRotator * 10);
+      ctx.lineTo(pieTracker * 15 * Math.tan(piRotator), piRotator * 15);
 
       rotator = Number(rotator.toFixed(4));
       rotatorTracker += rotator;
