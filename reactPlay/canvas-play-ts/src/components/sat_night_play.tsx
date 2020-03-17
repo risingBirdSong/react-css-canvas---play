153f51fe57bfc8c6ca17836e0dc1ff58193f794f
risingbirdsong <heregoingthere@outlook.com>
2020-03-17 11:18:27 -0700

    triangle swirl logo

--- reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
+++ reactPlay/canvas-play-ts/src/components/sat_night_play.tsx
@@ -30,7 +30,7 @@ const Canvassing = () => {
         piRotator = 0;
       }
 
-      if (rotatorTracker > 3.141592 * 20) {
+      if (rotatorTracker > 3.141592 * 16) {
         console.log("pieTracker", pieTracker, "rotatorTracker", rotatorTracker);
         return cancelAnimationFrame(requestId);
       }
@@ -47,8 +47,8 @@ const Canvassing = () => {
       //   Math.PI * 2
       // );
       ctx.lineTo(
-        (pieTracker * Math.abs(Math.log(Math.sin(piRotator)))) / 2,
-        (pieTracker * Math.abs(Math.log(Math.tan(piRotator)))) / 2
+        (pieTracker * Math.abs(Math.log(Math.cosh(piRotator)))) / 2,
+        (pieTracker * Math.abs(Math.log(Math.cosh(piRotator)))) / 2
       );
 
       rotator = Number(rotator.toFixed(4));
